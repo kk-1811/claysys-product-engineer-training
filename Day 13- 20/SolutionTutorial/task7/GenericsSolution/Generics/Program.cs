ff244a6175185512b3e0bc9b4159a777180f7e90
kishorek <kishorekandasamy1811@gmail.com>
2025-11-04 12:10:04 +0530

    Add files via upload

---
+++ Day 13- 20/SolutionTutorial/task7/GenericsSolution/Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test with integers
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
+
+            Console.WriteLine("Peek at the top of the integer stack: " + intStack.Peek());
+            Console.WriteLine("Popped from the integer stack: " + intStack.Pop());
+            Console.WriteLine("Peek again: " + intStack.Peek());
+
+            // Test with strings
+            Stack<string> stringStack = new Stack<string>();
+            stringStack.Push("User1");
+            stringStack.Push("User2");
+
+            Console.WriteLine("\nPeek at the top of the string stack: " + stringStack.Peek());
+            Console.WriteLine("Popped from the string stack: " + stringStack.Pop());
+            Console.WriteLine("Peek again: " + stringStack.Peek());
+        }
+    }
+}
---
+++ Day 13- 20/SolutionTutorial/task7/GenericsSolution/Generics/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task7
+{
+    public class Stack<T>
+    {
+        private List<T> elements = new List<T>();
+
+        public void Push(T item)
+        {
+            elements.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+            T item = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+            return elements[elements.Count - 1];
+        }
+    }
+}
