0e0e9d8d53fbf533a2b4219ccafd3fb23c223edf
kishorek <kishorekandasamy1811@gmail.com>
2025-11-04 18:47:27 +0530

    Add files via upload

---
+++ Day 13- 20/SolutionTutorial/task8/DelegatesSolution/Delegates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    public delegate int Calculate(int x, int y);
+
+    static void Main(string[] args)
+    {
+        Calculate add = new Calculate(Sum);
+        Calculate sub = new Calculate(Sub);
+        Calculate mul = new Calculate(Mul);
+        Calculate div = new Calculate(Div);
+
+
+        Console.WriteLine(add(10, 20));
+        Console.WriteLine(sub(10, 20));
+        Console.WriteLine(mul(5, 3));
+        Console.WriteLine(div(10, 2));
+
+
+    }
+
+    public static int Sum(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static int Sub(int x, int y)
+    {
+        return x - y;
+    }
+
+    public static int Mul(int x, int y)
+    {
+        return x * y;
+    }
+
+    public static int Div(int x, int y)
+    {
+        return x / y;
+    }
+
+
+
+}
